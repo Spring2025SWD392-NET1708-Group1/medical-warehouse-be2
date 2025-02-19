30c8d11a8ab73e8a91f00f6e355ce6ab43b4529d
Trungkien13 <kient9715@gmail.com>
2025-02-19 20:31:24 +0700

    Add LotCategoryService

---
+++ BLL/Interfaces/ILotCategoryService.cs
@@ -0,0 +1,19 @@
+﻿using BLL.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Interfaces
+{
+    public interface ILotCategoryService
+    {
+        Task<IEnumerable<LotCategoryViewDTO>> GetAllLotCategoriesAsync();
+        Task<LotCategoryViewDTO?> GetLotCategoryByIdAsync(Guid id);
+        Task<LotCategoryViewDTO> CreateLotCategoryAsync(LotCategoryCreateDTO lotCategoryDTO);
+        Task<bool> UpdateLotCategoryAsync(Guid id, LotCategoryUpdateDTO lotCategoryDTO);
+        Task<bool> DeleteLotCategoryAsync(Guid id);
+    }
+}
+
