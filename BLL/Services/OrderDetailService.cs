4372d2bb5d497fcc863faa931d819ea868c43b32
Phan Thành Nam <53371769+phnthnhnm@users.noreply.github.com>
2025-02-20 11:00:22 +0700

    refactor: do the same for OrderDetail

--- BLL/Services/OrderDetailService.cs
+++ BLL/Services/OrderDetailService.cs
@@ -1,64 +1,60 @@
 ﻿using AutoMapper;
 using BLL.DTOs;
 using BLL.Interfaces;
-using DAL.Context;
 using DAL.Entities;
-using Microsoft.EntityFrameworkCore;
+using DAL.Repositories.Interfaces;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace BLL.Services
 {
     public class OrderDetailService : IOrderDetailService
     {
-        private readonly AppDbContext _context;
+        private readonly IOrderDetailRepository _orderDetailRepository;
         private readonly IMapper _mapper;
-        public OrderDetailService(AppDbContext context, IMapper mapper)
+
+        public OrderDetailService(IOrderDetailRepository orderDetailRepository, IMapper mapper)
         {
-            _context = context;
+            _orderDetailRepository = orderDetailRepository;
             _mapper = mapper;
         }
 
         public async Task<OrderDetailViewDTO> CreateOrderDetail(OrderDetailCreateDTO orderDetailDTO)
         {
             var orderDetail = _mapper.Map<OrderDetail>(orderDetailDTO);
-            await _context.OrderDetails.AddAsync(orderDetail);
-            await _context.SaveChangesAsync();
+            await _orderDetailRepository.AddAsync(orderDetail);
             return _mapper.Map<OrderDetailViewDTO>(orderDetail);
         }
 
         public async Task<bool> DeleteOrderDetail(Guid id)
         {
-            var orderDetail = await _context.OrderDetails.FirstOrDefaultAsync(x => x.Id == id);
+            var orderDetail = await _orderDetailRepository.GetByIdAsync(id);
             if (orderDetail == null) return false;
 
-            _context.OrderDetails.Remove(orderDetail);
-            await _context.SaveChangesAsync();
+            await _orderDetailRepository.DeleteAsync(id);
             return true;
         }
 
         public async Task<IEnumerable<OrderDetailViewDTO>> GetAllOrderDetails()
         {
-            var orderDetails = await _context.OrderDetails.ToListAsync();
+            var orderDetails = await _orderDetailRepository.GetAllAsync();
             return _mapper.Map<IEnumerable<OrderDetailViewDTO>>(orderDetails);
         }
 
         public async Task<OrderDetailViewDTO?> GetOrderDetail(Guid id)
         {
-            var orderDetail = await _context.OrderDetails.FirstOrDefaultAsync(x=>x.Id == id);
-            return _mapper.Map<OrderDetailViewDTO?>(orderDetail);
+            var orderDetail = await _orderDetailRepository.GetByIdAsync(id);
+            return orderDetail != null ? _mapper.Map<OrderDetailViewDTO>(orderDetail) : null;
         }
 
         public async Task<bool> UpdateOrderDetail(Guid id, OrderDetailUpdateDTO orderDetailDTO)
         {
-            var orderDetail = await _context.OrderDetails.FirstOrDefaultAsync(x => x.Id == id);
+            var orderDetail = await _orderDetailRepository.GetByIdAsync(id);
             if (orderDetail == null) return false;
 
             _mapper.Map(orderDetailDTO, orderDetail);
-            await _context.SaveChangesAsync();
+            await _orderDetailRepository.UpdateAsync(orderDetail);
             return true;
         }
     }
