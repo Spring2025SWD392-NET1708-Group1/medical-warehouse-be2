1b0fadf554100a9c895c5abe1934917648998a53
XhuyZ <huylovengoc35@gmail.com>
2025-02-28 22:33:51 +0700

    solve mapper conflict

--- BLL/Mappers/AutoMapperProfile.cs
+++ BLL/Mappers/AutoMapperProfile.cs
@@ -4,84 +4,87 @@ using DAL.Entities;
 
 namespace BLL.Mappers
 {
-    public class AutoMapperProfile : Profile
+  public class AutoMapperProfile : Profile
+  {
+    public AutoMapperProfile()
     {
-        public AutoMapperProfile()
-        {
-            CreateMap<UserCreateDTO, User>()
-                .ForMember(dest => dest.PasswordHash, opt => opt.Ignore());
+      CreateMap<UserCreateDTO, User>()
+          .ForMember(dest => dest.PasswordHash, opt => opt.Ignore());
 
-            CreateMap<UserUpdateDTO, User>()
-                .ForAllMembers(opts => opts.Condition((src, dest, srcMember) =>
-                    srcMember != null));
+      CreateMap<UserUpdateDTO, User>()
+          .ForAllMembers(opts => opts.Condition((src, dest, srcMember) =>
+              srcMember != null));
 
-            CreateMap<User, UserViewDTO>()
-                .ForMember(dest => dest.RoleName, opt => opt.MapFrom(src => src.Role.Name));
+      CreateMap<User, UserViewDTO>()
+          .ForMember(dest => dest.RoleName, opt => opt.MapFrom(src => src.Role.Name));
 
-            CreateMap<RoleCreateDTO, Role>();
+      CreateMap<RoleCreateDTO, Role>();
 
-            CreateMap<RoleUpdateDTO, Role>()
-                .ForAllMembers(opts => opts.Condition((src, dest, srcMember) =>
-                    srcMember != null));
+      CreateMap<RoleUpdateDTO, Role>()
+          .ForAllMembers(opts => opts.Condition((src, dest, srcMember) =>
+              srcMember != null));
 
-            CreateMap<Role, RoleViewDTO>()
-                .ForMember(dest => dest.RoleId, opt => opt.MapFrom(src => src.Id));
+      CreateMap<Role, RoleViewDTO>()
+          .ForMember(dest => dest.RoleId, opt => opt.MapFrom(src => src.Id));
 
-            CreateMap<ItemCreateDTO, Item>()
-                .ForMember(dest => dest.ItemCategoryId, opt => opt.MapFrom(src => src.ItemCategoryId));
+      CreateMap<ItemCreateDTO, Item>()
+          .ForMember(dest => dest.ItemCategoryId, opt => opt.MapFrom(src => src.ItemCategoryId));
 
-            CreateMap<ItemUpdateDTO, Item>()
-                .ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null));
+      CreateMap<ItemUpdateDTO, Item>()
+          .ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null));
 
-            CreateMap<Item, ItemViewDTO>()
-                .ForMember(dest => dest.CategoryName, opt => opt.MapFrom(src => src.ItemCategory.Name));
+      CreateMap<Item, ItemViewDTO>()
+          .ForMember(dest => dest.CategoryName, opt => opt.MapFrom(src => src.ItemCategory.Name));
 
-            CreateMap<ItemViewDTO, Item>()
-                .ForPath(dest => dest.ItemCategory.Name, opt => opt.MapFrom(src => src.CategoryName));
 
-            CreateMap<ItemCategory, ItemCategoryDTO>()
-                .ReverseMap()
-                .ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null));
+      CreateMap<ItemCategory, ItemCategoryDTO>()
+          .ReverseMap()
+          .ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null));
+      CreateMap<ItemViewDTO, Item>()
+          .ForPath(dest => dest.ItemCategory.Name, opt => opt.MapFrom(src => src.CategoryName));
 
-            CreateMap<Order, OrderViewDTO>()
-                .ForMember(dest => dest.OrderDetails, opt => opt.MapFrom(src => src.OrderDetails))
-                .ForMember(dest => dest.Username, opt => opt.MapFrom(src => src.User.FullName));
+      CreateMap<ItemCategory, ItemCategoryDTO>()
+          .ReverseMap()
+          .ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null));
 
-            CreateMap<OrderCreateDTO, Order>()
-                .ForMember(dest => dest.OrderDetails, opt => opt.MapFrom(src => src.OrderDetails));
+      CreateMap<Order, OrderViewDTO>()
+          .ForMember(dest => dest.OrderDetails, opt => opt.MapFrom(src => src.OrderDetails))
+          .ForMember(dest => dest.Username, opt => opt.MapFrom(src => src.User.FullName));
 
-            CreateMap<OrderUpdateDTO, Order>()
-                .ForAllMembers(opts => opts.Condition((src, dest, srcMember) =>
-                    srcMember != null));
+      CreateMap<OrderCreateDTO, Order>()
+          .ForMember(dest => dest.OrderDetails, opt => opt.MapFrom(src => src.OrderDetails));
 
-            CreateMap<OrderDetailCreateDTO, OrderDetail>();
+      CreateMap<OrderUpdateDTO, Order>()
+          .ForAllMembers(opts => opts.Condition((src, dest, srcMember) =>
+              srcMember != null));
 
-            CreateMap<OrderDetailUpdateDTO, OrderDetail>()
-                .ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null));
+      CreateMap<OrderDetailCreateDTO, OrderDetail>();
 
-            CreateMap<OrderDetail, OrderDetailViewDTO>()
-                .ForPath(dest => dest.OrderDate, opt => opt.MapFrom(src => src.Order.OrderDate))
-                .ForMember(dest => dest.ItemName, opt => opt.MapFrom(src => src.Item.Name))
-                .ForMember(dest => dest.UnitPrice, opt => opt.MapFrom(src => src.Price));
+      CreateMap<OrderDetail, OrderDetailViewDTO>()
+          .ForPath(dest => dest.OrderDate, opt => opt.MapFrom(src => src.Order.OrderDate))
+          .ForMember(dest => dest.ItemName, opt => opt.MapFrom(src => src.Item.Name))
+          .ForMember(dest => dest.UnitPrice, opt => opt.MapFrom(src => src.Price));
+      CreateMap<Submission, SubmissionViewDTO>()
+          .ForPath(dest => dest.UserName, opt => opt.MapFrom(src => src.User.FullName))
+          .ForMember(dest => dest.SubmittedAt, opt => opt.MapFrom(src => src.CreatedDate));
 
-            CreateMap<SubmissionCreateDTO, Submission>();
+      CreateMap<LotRequest, LotRequestViewDTO>()
+          .ForMember(dest => dest.Item, opt => opt.MapFrom(src => src.Item))
+          .ForPath(dest => dest.StaffName, opt => opt.MapFrom(src => src.Staff.FullName));
 
-            CreateMap<SubmissionUpdateDTO, Submission>()
-                .ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null));
+      CreateMap<Submission, SubmissionViewDTO>()
+          .ForPath(dest => dest.UserName, opt => opt.MapFrom(src => src.User.FullName))
+          .ForMember(dest => dest.SubmittedAt, opt => opt.MapFrom(src => src.CreatedDate));
 
-            CreateMap<Submission, SubmissionViewDTO>()
-                .ForPath(dest => dest.UserName, opt => opt.MapFrom(src => src.User.FullName))
-                .ForMember(dest => dest.SubmittedAt, opt => opt.MapFrom(src => src.CreatedDate));
+      CreateMap<LotRequest, LotRequestViewDTO>()
+          .ForMember(dest => dest.Item, opt => opt.MapFrom(src => src.Item))
+          .ForPath(dest => dest.StaffName, opt => opt.MapFrom(src => src.Staff.FullName));
 
-            CreateMap<LotRequest, LotRequestViewDTO>()
-                .ForMember(dest => dest.Item, opt => opt.MapFrom(src => src.Item))
-                .ForPath(dest => dest.StaffName, opt => opt.MapFrom(src => src.Staff.FullName));
+      CreateMap<LotRequestCreateDTO, LotRequest>();
 
-            CreateMap<LotRequestCreateDTO, LotRequest>();
+      CreateMap<LotRequestUpdateDTO, LotRequest>()
+          .ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null));
 
-            CreateMap<LotRequestUpdateDTO, LotRequest>()
-                .ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null));
-
-        }
     }
+  }
 }
