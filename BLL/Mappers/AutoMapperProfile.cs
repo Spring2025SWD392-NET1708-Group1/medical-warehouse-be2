02bf4cb9cf7d57272d726607edfc86d9c4e73804
P4nzerr <enderthelord@gmail.com>
2025-03-02 23:40:34 +0700

    chore : map storage name into ItemViewDTO

--- BLL/Mappers/AutoMapperProfile.cs
+++ BLL/Mappers/AutoMapperProfile.cs
@@ -35,7 +35,8 @@ namespace BLL.Mappers
                 .ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null));
 
             CreateMap<Item, ItemViewDTO>()
-                .ForMember(dest => dest.CategoryName, opt => opt.MapFrom(src => src.ItemCategory.Name));
+                .ForMember(dest => dest.CategoryName, opt => opt.MapFrom(src => src.ItemCategory.Name))
+                .ForMember(dest => dest.StorageName, opt => opt.MapFrom(src => src.Storage.Name));
 
             // Item Category Mappings
             CreateMap<ItemCategory, ItemCategoryDTO>()
